467ee6c531c996657d45d848793fb74c36ef6d3c
Harika <harika.duyu@gmail.com>
2021-09-14 21:12:16 +0200

    use write to file instead

--- server.py
+++ server.py
@@ -8,10 +8,6 @@ from datetime import datetime, timedelta
 HOST = 'localhost'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
-def print_stdout(queue):
-    sys.stdout = open('output.txt', 'a')
-    print(queue)
-    sys.stdout.close()
 
 if __name__ == "__main__":
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -32,9 +28,8 @@ if __name__ == "__main__":
 
                     elapsed = start - datetime.now()
                     if elapsed > timedelta(seconds=10) or len(queue) > 10:
-                        print(queue)
-                        for q in queue:
-                            print_stdout(q)
+                        with open('output.txt', 'a') as file:
+                            file.write(json.dumps(queue))
                         queue = []
                         start = datetime.now()
 
